2fa1cbbd504e7562aab2b39c66510a4d11c622c5
IsmaelCesar <leamscesar@gmail.com>
2018-08-10 22:25:52 -0300

    Movimentando para cima

--- personagens.asm
+++ personagens.asm
@@ -13,20 +13,54 @@
 
 ################ Movimentos Básicos #####################
 #### Procedimento para mover um peronagem para cima
-# $a0 -> endereço contendo a posição do personagem anderior
+# $a0 -> endereço da célula contendo a posição do personagem anderior
 # $a1 -> Cor do personagem
 # $a2 -> Bitmap address
+############# Retorna a posição atualizada#############
+# $v0 -> Posição da célula atualizada
 .macro mover_para_cima(%posIni,%corPers)
 	add $a0,$zero,%posIni
-	add $a1,$zero,%corPers
-	lw  $a2,$zero,bitmap_address
+	lw  $a1,%corPers
+	lw  $a2,bitmap_address
 	jal mover_para_cima_function
 .end_macro
 mover_para_cima_function:
 
+	addi $t0,$a0,0     #Salvando endereço da célula em temporário
+	addi $t1,$a1,0     #Salvando cor do personagem em temporário
+	add $t2,$a2,$zero   #Somando o valor a o endereço base
+	lw   $t3, corPreta #Carregando cor preta no registrador	
+
+	add $t4,$a2,$t0    #Carregando endereço da célula junto com o base em $t4
+	sw $t3,0($t4)      #Salvando a cor preta no endereço antigo
+	
+	##Dando um tempo de 0,5 segundos
+	li $v0,32
+	addi $a0,$zero,500 
+	syscall
+	
+	addi $t0,$t0,-256 #Movendo pra sima
+	add $t2,$t2,$t0 
+	sw   $t1,0($t2)
+	
+	addi $v0,$t0,0    #retornando o endereço da célula	
+	jr $ra
+	
+#### Procedimento para mover um peronagem para cima
+# $a0 -> endereço contendo a posição do personagem anderior
+# $a1 -> Cor do personagem
+# $a2 -> Bitmap address
+.macro mover_para_baixo(%posIni,%corPers)
+	add $a0,$zero,%posIni
+	lw  $a1,$zero,%corPers
+	lw  $a2,$zero,bitmap_address
+	jal mover_para_baixo_function
+.end_macro
+mover_para_baixo_function:
+
 	addi $t0,$a0,0     #Salvando endereço em temporário
 	addi $t1,$a1,0     #Salvando cor do personagem em temporário
-	addi $t0,$a2,$t0   #Somando o valor a o endereço base
+	add $t0,$a2,$t0   #Somando o valor a o endereço base
 	lw   $t3, corPreta #Carregando cor preta no registrador
 	
 	
@@ -34,12 +68,12 @@ mover_para_cima_function:
 	
 	##Dando um tempo de 0,5 segundos
 	li $v0,32
-	addi $a0,500 
+	add $a0,$zero,500 
 	syscall
 	
-	addi $t0,$t0,-256 #Movendo pra sima
+	addi $t0,$t0,256 #Movendo pra sima
 	sw $t1,0($t0)
 	
-	jr $ra
+	jr $ra	
 	
 
